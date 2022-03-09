adeb9cca2148b359d83bf645cb6d755cf3c9a29d
Ellie Foote <eleanorjfoote@gmail.com>
2022-03-09 12:38:29 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+      title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-QL5JM5Q3YF"
+    }
+  }]
+};
