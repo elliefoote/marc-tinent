59326a5b00ec7b61b2a027f1e0adb5751fa53fe5
Ellie Foote <eleanorjfoote@gmail.com>
2022-04-04 18:25:29 +0200

    migrated new web

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,33 @@
-import * as React from "react";
+import React from "react";
+import Navbar from '../components/Navbar';
+import Cover from '../components/Cover';
+import Bookcard from '../components/Bookcard';
+import Contact from '../components/Contact';
+import './newwebsite.css';
 import './index.css';
-import {BsInstagram} from 'react-icons/bs';
-import {AiTwotoneMail} from 'react-icons/ai';
 
-// markup
 const IndexPage = () => {
-  return (
+    return (
+  
+        
+        <div id="new-web" className="bg-light">
+            <header>
+                <Navbar />
+            </header>
 
-      
-      <main>
-        <title>Marc Tinent | Escritor de Cosas</title>
-        <div id="hero-overlay" className="d-flex justify-content-center align-items-center">
-            <div id="hero-text-container" className="d-flex flex-column justify-content-center align-items-center">
-            <h1 className="display-2 text-light text-center fw-bold">¡Nueva Web Muy Pronto!</h1>
-            <p id="my-p" className="text-center text-light fs-3">Escribo cosas. <br />Sígueme en Instagram para descubrirlas y ver consejos de escritura o contáctame por mail.</p>
-            <div className="d-flex mt-3">
-            <div className="my-social-icon d-flex align-items-center justify-content-center me-2"><a href="http://www.instagram.com/marctinent"><BsInstagram className="fs-3"/></a></div>
-            <div className="my-social-icon d-flex align-items-center justify-content-center ms-2"><a href="mailto:marctinent@gmail.com"><AiTwotoneMail className="fs-2" /></a></div>
+            <Cover />
+
+            <div id="latest-work" className="page-section container d-flex flex-column">
+                <h2 className="section-head">LO ÚLTIMO</h2>
+                <Bookcard />
             </div>
-            </div>
+
+            <Contact />
+
         </div>
-      </main>
-
-  )
-}
-
-export default IndexPage
+  
+    )
+  }
+  
+  export default IndexPage
+  
---
+++ src/pages/oldindex.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import './index.css';
+import {BsInstagram} from 'react-icons/bs';
+import {AiTwotoneMail} from 'react-icons/ai';
+
+// markup
+const OldIndexPage = () => {
+  return (
+
+      
+      <main>
+        <title>Marc Tinent | Escritor de Cosas</title>
+        <div id="hero-overlay" className="d-flex justify-content-center align-items-center">
+            <div id="hero-text-container" className="d-flex flex-column justify-content-center align-items-center">
+            <h1 className="display-2 text-light text-center fw-bold">¡Nueva Web Muy Pronto!</h1>
+            <p id="my-p" className="text-center text-light fs-3">Escribo cosas. <br />Sígueme en Instagram para descubrirlas y ver consejos de escritura o contáctame por mail.</p>
+            <div className="d-flex mt-3">
+            <div className="my-social-icon d-flex align-items-center justify-content-center me-2"><a href="http://www.instagram.com/marctinent"><BsInstagram className="fs-3"/></a></div>
+            <div className="my-social-icon d-flex align-items-center justify-content-center ms-2"><a href="mailto:marctinent@gmail.com"><AiTwotoneMail className="fs-2" /></a></div>
+            </div>
+            </div>
+        </div>
+      </main>
+
+  )
+}
+
+export default OldIndexPage
