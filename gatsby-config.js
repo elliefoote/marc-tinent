b0d7480710cc7d1005625622618df5440bf76880
Ellie Foote <eleanorjfoote@gmail.com>
2022-12-14 16:01:15 +0100

    rm preact due to incompatible with react 18

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,31 @@
 module.exports = {
   siteMetadata: {
-      title: `Marc Tinent | Escritor de Cosas`,
-    siteUrl: `https://www.marctinent.com`
+    title: `Marc Tinent | Escritor de Cosas`,
+    siteUrl: `https://www.marctinent.com`,
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-QL5JM5Q3YF"
-    }
-  },
-  `gatsby-plugin-preact`, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-QL5JM5Q3YF",
+      },
     },
-    __key: "images"
-  },
-  {
-    resolve: 'gatsby-plugin-html-attributes',
-    options: {
-      lang: 'es'
-    }
-  }
-]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-plugin-html-attributes",
+      options: {
+        lang: "es",
+      },
+    },
+  ],
 };
