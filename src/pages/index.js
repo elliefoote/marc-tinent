b633b2f045f799c982a81c406cc129e1d6524248
Ellie Foote <eleanorjfoote@gmail.com>
2023-02-02 20:38:41 +0100

    lol

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = ({ data }) => {
           spacing={{ sm: 1, md: 2 }}
           defaultColumns={2}
           defaultSpacing={2}
-          defaultHeight={2000}
+          defaultHeight={3000}
         >
           <Cover />
           <Headshot />
