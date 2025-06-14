4dce50a9b8692d78902aed2a83528c98a3cded13
Ellie Foote <eleanorjfoote@gmail.com>
2025-06-14 15:57:20 +1000

    fix description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Marc Tinent | Escritor de Cosas",
-  description: "Marc Tinent es un escritor de cosas. Aquí encontrarás sus libros, programas de televisión y más.",
+  description: "Soy Marc Tinent. Escribo cosas. Como novelas, tebeos, series, programas de entretenimiento para TV y copy de todo tipo. Y si me pongo, hasta postales de Navidad.",
 };
 
 export default function RootLayout({
